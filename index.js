20253dd1d86b73cbf0f0b8b75a58164d53116c4f
tgrall-dev <92017672+tgrall-dev@users.noreply.github.com>
2022-10-19 17:25:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
-// Callin my API
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/some/path', function(req, res) {
+    let url = req.param("url");
+    // BAD: the host of `url` may be controlled by an attacker
+    if (url.includes("example.com")) {
+        res.redirect(url);
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 console.log("Hello world");
